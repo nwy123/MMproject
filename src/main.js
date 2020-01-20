6bf9e8c698ef2ff63be6826e9a25853cd7ae4be9
nwy <1094616318@qq.com>
2020-01-20 20:36:11 +0800

    初始化-整合element-ui

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 
 import router from './router/index'
 Vue.config.productionTip = false
-
+//导入element-ui
+import ElementUI from 'element-ui'
+Vue.use(ElementUI)
 new Vue({
   render: h => h(App),
   router
